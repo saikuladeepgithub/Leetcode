0d210f1b70ddfa87adfc25f67a6b2cbefd120891
saikuladeepgithub <141644207+saikuladeepgithub@users.noreply.github.com>
2024-03-19 15:31:31 +0530

    Time: 5 ms (17.72%), Space: 9.8 MB (33.70%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -11,36 +11,58 @@ public:
         //             swap(nums[i],nums[j]);
         //         }
         //     }
+        // // }
+        // int count0=0;
+        // int count1=0;
+        // int count2=0;
+        // for(int i=0;i<nums.size();i++)
+        // {
+        //     if(nums[i]==0)
+        //     {
+        //         count0=count0+1;
+        //     }
+        //     else if(nums[i]==1)
+        //     {
+        //         count1=count1+1;
+        //     }
+        //     else
+        //     {
+        //         count2=count2+1;
+        //     }
         // }
-        int count0=0;
-        int count1=0;
-        int count2=0;
-        for(int i=0;i<nums.size();i++)
+        // for(int i=0;i<count0;i++)
+        // {
+        //     nums[i]=0;
+        // }
+        // for(int i=count0;i<count0+count1;i++)
+        // {
+        //     nums[i]=1;
+        // }
+        // for(int i=count0+count1;i<nums.size();i++)
+        // {
+        //     nums[i]=2;
+        // }
+        // Dutch national flag algorithm
+        int low=0;
+        int mid=0;
+        int high=nums.size()-1;
+        while(mid<=high)
         {
-            if(nums[i]==0)
+            if(nums[mid]==0)
             {
-                count0=count0+1;
+                swap(nums[low],nums[mid]);
+                low=low+1;
+                mid=mid+1;
             }
-            else if(nums[i]==1)
+            else if(nums[mid]==1)
             {
-                count1=count1+1;
+                mid=mid+1;
             }
             else
             {
-                count2=count2+1;
+                swap(nums[mid],nums[high]);
+                high=high-1;
             }
-        }
-        for(int i=0;i<count0;i++)
-        {
-            nums[i]=0;
-        }
-        for(int i=count0;i<count0+count1;i++)
-        {
-            nums[i]=1;
-        }
-        for(int i=count0+count1;i<nums.size();i++)
-        {
-            nums[i]=2;
         }
     }
 };
