b74159b009b54ceef2c800747cd4f6be8e80ae9a
saikuladeepgithub <141644207+saikuladeepgithub@users.noreply.github.com>
2024-03-19 14:49:50 +0530

    Time: 4 ms (31.06%), Space: 9.9 MB (33.70%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -2,15 +2,45 @@
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
+        // for(int i=0;i<nums.size();i++)
+        // {
+        //     for(int j=i;j<nums.size();j++)
+        //     {
+        //         if(nums[i]>nums[j])
+        //         {
+        //             swap(nums[i],nums[j]);
+        //         }
+        //     }
+        // }
+        int count0=0;
+        int count1=0;
+        int count2=0;
         for(int i=0;i<nums.size();i++)
         {
-            for(int j=i;j<nums.size();j++)
+            if(nums[i]==0)
             {
-                if(nums[i]>nums[j])
-                {
-                    swap(nums[i],nums[j]);
-                }
+                count0=count0+1;
             }
+            else if(nums[i]==1)
+            {
+                count1=count1+1;
+            }
+            else
+            {
+                count2=count2+1;
+            }
+        }
+        for(int i=0;i<count0;i++)
+        {
+            nums[i]=0;
+        }
+        for(int i=count0;i<count0+count1;i++)
+        {
+            nums[i]=1;
+        }
+        for(int i=count0+count1;i<nums.size();i++)
+        {
+            nums[i]=2;
         }
     }
 };
