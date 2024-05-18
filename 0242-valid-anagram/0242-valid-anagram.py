8042f96ab0ff3a23bc863aa1a7d9e2d65d1808ad
saikuladeepgithub <141644207+saikuladeepgithub@users.noreply.github.com>
2024-05-18 17:39:58 +0530

    Time: 43 ms (19.66%), Space: 12.8 MB (50.23%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -6,3 +6,13 @@ class Solution(object):
         :rtype: bool
         """
         return sorted(s)==sorted(t)
+        sd={}
+        td={}
+        for i in s:
+            sd[i]=sd[i]+1
+        for i in t:
+            td[i]=td[i]+1
+        for i in s:
+            if(sd[i]!=td[i]):
+                return False
+        return True
