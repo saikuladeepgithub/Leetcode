2214cd82b0b079bca0408e8e3907481cf3b7260b
saikuladeepgithub <141644207+saikuladeepgithub@users.noreply.github.com>
2024-06-01 19:21:54 +0530

    Time: 6 ms (70.18%), Space: 12.5 MB (12.66%) - LeetHub

--- 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.cpp
+++ 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.cpp
@@ -9,24 +9,36 @@
 class Solution {
 public:
     ListNode *detectCycle(ListNode *head) {
-        ListNode * tail=head;
-        vector<ListNode*> nodePointers;
-        if(head==NULL)
+        // ListNode * tail=head;
+        // vector<ListNode*> nodePointers;
+        // if(head==NULL)
+        // {
+        //     return NULL;
+        // }
+        // do
+        // {
+        //     nodePointers.push_back(tail);
+        //     tail=tail->next;
+        //     for(int i=0;i<nodePointers.size();i++)
+        //     {
+        //         if(nodePointers[i]==tail)
+        //         {
+        //             return tail;
+        //         }
+        //     }
+        // }while(tail!=NULL);
+        // return NULL;
+        map <ListNode *,int> mpp;
+        ListNode * temp=head;
+        while(temp!=NULL)
         {
-            return NULL;
-        }
-        do
-        {
-            nodePointers.push_back(tail);
-            tail=tail->next;
-            for(int i=0;i<nodePointers.size();i++)
+            if(mpp.find(temp)!=mpp.end())
             {
-                if(nodePointers[i]==tail)
-                {
-                    return tail;
-                }
+                return temp;
             }
-        }while(tail!=NULL);
+            mpp[temp]==1;
+            temp=temp->next;
+        }
         return NULL;
     }
 };
