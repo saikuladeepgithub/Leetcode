63a153eeccb4133ed0012c5eb34005fbff77c7b4
saikuladeepgithub <141644207+saikuladeepgithub@users.noreply.github.com>
2024-06-07 23:53:02 +0530

    Time: 555 ms (18.25%), Space: 298.3 MB (39.39%) - LeetHub

--- 2095-delete-the-middle-node-of-a-linked-list/2095-delete-the-middle-node-of-a-linked-list.cpp
+++ 2095-delete-the-middle-node-of-a-linked-list/2095-delete-the-middle-node-of-a-linked-list.cpp
@@ -11,32 +11,46 @@
 class Solution {
 public:
     ListNode* deleteMiddle(ListNode* head) {
-        int l=0;
-        ListNode * temp=head;
-        if(head->next==NULL)
-        {
-            return NULL;
-        }
-        while(temp!=NULL)
-        {
-            l=l+1;
-            temp=temp->next;
-        }
-        l=l/2;
-        int count=0;
-        temp=head;
+        // int l=0;
+        // ListNode * temp=head;
+        // if(head->next==NULL)
+        // {
+        //     return NULL;
+        // }
+        // while(temp!=NULL)
+        // {
+        //     l=l+1;
+        //     temp=temp->next;
+        // }
+        // l=l/2;
+        // int count=0;
+        // temp=head;
+        // ListNode * prev=NULL;
+        // while(temp!=NULL)
+        // {
+        //     if(count==l)
+        //     {
+        //         prev->next=temp->next;
+        //         return head;
+        //     }
+        //     prev=temp;
+        //     temp=temp->next;
+        //     count=count+1;
+        // }
+        // return head;
+        
+        ListNode * slow,*fast;
+        if(head==NULL || head->next==NULL) return NULL;
+        slow=head;
+        fast=head;
         ListNode * prev=NULL;
-        while(temp!=NULL)
+        while(fast!=NULL && fast->next!=NULL)
         {
-            if(count==l)
-            {
-                prev->next=temp->next;
-                return head;
-            }
-            prev=temp;
-            temp=temp->next;
-            count=count+1;
+            prev=slow;
+            slow=slow->next;
+            fast=fast->next->next;
         }
+        prev->next=slow->next;
         return head;
     }
 };
