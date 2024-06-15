f75275ea6997a230c7d86378797ef36a03d5f2b5
saikuladeepgithub <141644207+saikuladeepgithub@users.noreply.github.com>
2024-06-15 23:34:54 +0530

    Time: 9757 ms (5.00%), Space: 16.7 MB (27.34%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.py
@@ -1,35 +1,9 @@
-class Solution(object):
-    def longestPalindrome(self, s):
-        """
-        :type s: str
-        :rtype: str
-        """
-        # def pal(s):
-        #     if(s==s[::-1]):
-        #         return True
-        #     return False
-        # res=''
-        # for i in range(len(s)):
-        #     for j in range(i,len(s)):
-        #         sub=s[i:j+1]
-        #         if(pal(sub)):
-        #             if(len(sub)>len(res)):
-        #                 res=sub
-        # return res
-        if(len(s)==0):
-            return ''
-        def expand(s,left,right):
-            while(left>=0 and right<len(s) and s[left]==s[right]):
-                left=left-1
-                right=right+1
-            return s[left+1:right]
-        longest=''
+class Solution:
+    def longestPalindrome(self, s: str) -> str:
+        ans=''
         for i in range(len(s)):
-            odd=expand(s,i,i)
-            if(len(odd)>len(longest)):
-                longest=odd
-            even=expand(s,i,i+1)
-            if(len(even)>len(longest)):
-                longest=even
-        return longest
-            
+            for j in range(i,len(s)):
+                if(s[i:j+1]==s[i:j+1][::-1]):
+                    if(len(s[i:j+1])>len(ans)):
+                        ans=s[i:j+1]
+        return ans
