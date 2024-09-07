3951be6e6f44053b31e48e3176d6148a2cd9335e
saikuladeepgithub <141644207+saikuladeepgithub@users.noreply.github.com>
2024-09-07 23:39:46 +0530

    Time: 0 ms (100%), Space: 7.8 MB (93.38%) - LeetHub

--- 3553-check-if-two-chessboard-squares-have-the-same-color/3553-check-if-two-chessboard-squares-have-the-same-color.cpp
+++ 3553-check-if-two-chessboard-squares-have-the-same-color/3553-check-if-two-chessboard-squares-have-the-same-color.cpp
@@ -4,5 +4,12 @@ public:
         int one = (coordinate1[0] - 'a') + (coordinate1[1] - '0');
         int two = (coordinate2[0] - 'a') + (coordinate2[1]-'0');
         return one % 2 == two % 2;
+
+        // Each square's position can be translated into a unique numeric value:
+// The column (a-h) can be mapped to integers 0-7.
+// The row (1-8) can be directly used as integers.
+// The color of a square can be determined by summing its column and row indices:
+// If the sum is even, the square is black.
+// If the sum is odd, the square is white
     }
 };
