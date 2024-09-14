8f0cebc23b018bdad919e77ef14d34083ba6b6a9
saikuladeepgithub <141644207+saikuladeepgithub@users.noreply.github.com>
2024-09-14 13:41:11 +0530

    Time: 269 ms (45.4%), Space: 16.7 MB (44.12%) - LeetHub

--- 0037-sudoku-solver/0037-sudoku-solver.py
+++ 0037-sudoku-solver/0037-sudoku-solver.py
@@ -9,41 +9,39 @@ class Solution:
                 if board[i][c] == k or board[r][i] == k:
                     return False
                 
-                # Check the 3x3 subgrid
+                # Check 3x3 subgrid
                 subgrid_row = 3 * (r // 3) + i // 3
                 subgrid_col = 3 * (c // 3) + i % 3
                 if board[subgrid_row][subgrid_col] == k:
                     return False
             return True    
 
-        # Recursively fill the board
         def fill(r, c):
-            # If we reach the end of the board, the solution is found
+            # If we have reached the end of the board, return True
             if r == 9:
                 return True
             
-            # Move to the next row if we reach the end of the current row
+            # Move to the next row if the column is completed
             if c == 9:
                 return fill(r + 1, 0)
             
-            # If the current cell is empty, try filling it
+            # If the current cell is empty, try placing numbers
             if board[r][c] == '.':
                 for k in range(1, 10):
                     if isValid(r, c, str(k)):
                         board[r][c] = str(k)
                         
-                        # Recur for the next cell
+                        # If the board is solved with this number, return True
                         if fill(r, c + 1):
                             return True
                         
-                        # Backtrack if placing 'k' doesn't lead to a solution
+                        # If the placement didn't work, reset the cell and try the next number
                         board[r][c] = '.'
                 
-                # Return False if no valid number can be placed
                 return False
             
-            # Move to the next cell if the current one is already filled
+            # If the cell is already filled, move to the next column
             return fill(r, c + 1)
 
-        # Start solving from the top-left corner
+        # Start solving the board from the first cell
         fill(0, 0)
