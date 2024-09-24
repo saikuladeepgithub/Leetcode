0545b73981330e7f99707c76139ba14d022807a3
saikuladeepgithub <141644207+saikuladeepgithub@users.noreply.github.com>
2024-09-24 19:12:36 +0530

    Time: 193 ms (35.84%), Space: 11 MB (79.07%) - LeetHub

--- 0494-target-sum/0494-target-sum.cpp
+++ 0494-target-sum/0494-target-sum.cpp
@@ -1,15 +1,46 @@
 class Solution {
 public:
-    int func(vector<int> & nums,int sum,int i)
+    int difference(int i,int sum,vector<int>&nums)
     {
-        if(i<0 && sum==0) return 1;
-        if(i<0) return 0;
-        int plus = 0;
-        plus = func(nums,sum-nums[i],i-1);
-        int minus = func(nums,sum+nums[i],i-1);
-        return plus+minus;
+        if(i==0)
+        {
+            if(sum==0 && nums[0]==0) return 2;
+            if(sum-nums[0]==0 || sum==0) return 1;
+            return 0;
+        }
+        int notpick = difference(i-1,sum,nums);
+
+        int pick = 0;
+
+        if(nums[i]<=sum)
+        {
+            pick = difference(i-1,sum-nums[i],nums);
+        }
+
+        return pick+notpick;
     }
+    // int func(vector<int> & nums,int sum,int i)
+    // {
+    //     if(i<0 && sum==0) return 1;
+    //     if(i<0) return 0;
+    //     int plus = 0;
+    //     plus = func(nums,sum-nums[i],i-1);
+    //     int minus = func(nums,sum+nums[i],i-1);
+    //     return plus+minus;
+    // }
     int findTargetSumWays(vector<int>& nums, int target) {
-        return func(nums,target,nums.size()-1);
+        // return func(nums,target,nums.size()-1);
+
+        int sum = 0;
+        int n = nums.size();
+        for(int i=0;i<n;i++)
+        {
+            sum=sum+nums[i];
+        }
+
+        if((sum-target)%2!=0) return 0;
+        int s2 = (sum-target)/2;
+        return difference(n-1,s2,nums);
+
     }
 };
