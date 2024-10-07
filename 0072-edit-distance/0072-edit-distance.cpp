4b76ab770deb4107b5eea2334f62dadec8a1dd40
saikuladeepgithub <141644207+saikuladeepgithub@users.noreply.github.com>
2024-10-07 20:40:39 +0530

    Time: 3 ms (93.43%), Space: 10.4 MB (94.63%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -19,36 +19,66 @@ public:
 
         // tabulation part
 
-        vector<vector<int>> dp(n+1,vector<int>(m+1,0));
-        //base case
+        // vector<vector<int>> dp(n+1,vector<int>(m+1,0));
 
-        for(int i=0;i<=n;i++)
-        {
-            dp[i][0] = i;
-        }
+        // //base case
+
+        // for(int i=0;i<=n;i++)
+        // {
+        //     dp[i][0] = i;
+        // }
+        // for(int j=0;j<=m;j++)
+        // {
+        //     dp[0][j] = j;
+        // }
+
+        // for(int i=1;i<=n;i++)
+        // {
+        //     for(int j=1;j<=m;j++)
+        //     {
+        //         if(word1[i-1]==word2[j-1])
+        //         {
+        //             dp[i][j] = dp[i-1][j-1];
+        //         }
+        //         else
+        //         {
+        //             dp[i][j] = 1 + min(dp[i-1][j],
+        //             min(dp[i][j-1],dp[i-1][j-1]));
+        //         }
+        //     }
+        // }
+
+        // return dp[n][m];
+        
+
+        // space optimisation
+
+        vector<int>prev(m+1,0),cur(m+1,0);
+
+        // base case
         for(int j=0;j<=m;j++)
         {
-            dp[0][j] = j;
+            prev[j] = j;
         }
 
         for(int i=1;i<=n;i++)
         {
+            cur[0] = i;
             for(int j=1;j<=m;j++)
             {
                 if(word1[i-1]==word2[j-1])
                 {
-                    dp[i][j] = dp[i-1][j-1];
+                    cur[j] = prev[j-1];
                 }
                 else
                 {
-                    dp[i][j] = 1 + min(dp[i-1][j],
-                    min(dp[i][j-1],dp[i-1][j-1]));
+                    cur[j] = 1 + min(prev[j],
+                    min(cur[j-1],prev[j-1]));
                 }
             }
+            prev = cur;
         }
 
-        return dp[n][m];
-        
-
+        return prev[m];
     }
 };
