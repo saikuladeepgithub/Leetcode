5c74507b785f73d7b2b5405f216a68e77e88f55e
saikuladeepgithub <141644207+saikuladeepgithub@users.noreply.github.com>
2024-10-11 16:59:24 +0530

    Time: 4 ms (55.61%), Space: 14.5 MB (54.14%) - LeetHub

--- 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
+++ 0309-best-time-to-buy-and-sell-stock-with-cooldown/0309-best-time-to-buy-and-sell-stock-with-cooldown.cpp
@@ -25,14 +25,20 @@ public:
         vector<vector<int>> dp(n+2,vector<int>(2,0));
         // return func(0,1,prices,dp);
 
+        vector<int> front2(2,0);
+        vector<int> front1(2,0);
+        vector<int> cur(2,0);
 
         for(int ind=n-1;ind>=0;ind--)
         {
-            dp[ind][1] = max(-prices[ind]+dp[ind+1][0],dp[ind+1][1]);
+            cur[1] = max(-prices[ind]+front1[0],front1[1]);
 
-            dp[ind][0] = max(prices[ind]+dp[ind+2][1],dp[ind+1][0]);
+            cur[0] = max(prices[ind]+front2[1],front1[0]);
+
+            front2 = front1;
+            front1 = cur;
         }
 
-        return dp[0][1];
+        return cur[1];
     }
 };
