d07b87c5c195acfb7def20bb73ced0d9836e4256
saikuladeepgithub <141644207+saikuladeepgithub@users.noreply.github.com>
2024-12-03 16:42:26 +0530

    Time: 0 ms (100%), Space: 23.3 MB (5.89%) - LeetHub

---
+++ 0735-asteroid-collision/0735-asteroid-collision.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<int> asteroidCollision(vector<int>& asteroids) {
+        list<int> st;
+
+        int n = asteroids.size();
+
+        for(int i=0;i<n;i++)
+        {
+            if(asteroids[i]>0) st.push_back(asteroids[i]);
+            else
+            {
+                while(!st.empty() && st.back()>0 && st.back()<abs(asteroids[i]))
+                {
+                    st.pop_back();
+                }   
+                if(!st.empty() && st.back()==abs(asteroids[i])) st.pop_back();
+                else if(st.empty() || st.back()<0) st.push_back(asteroids[i]);
+            }
+        }
+        vector<int> res(st.begin(),st.end());
+        return res;
+    }
+};
