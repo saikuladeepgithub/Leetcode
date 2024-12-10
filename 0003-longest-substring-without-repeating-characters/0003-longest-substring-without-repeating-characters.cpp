8de648d39824d839dd0d3a24e62b85e67aaeb4a1
saikuladeepgithub <141644207+saikuladeepgithub@users.noreply.github.com>
2024-12-10 08:32:19 +0530

    Time: 17 ms (39.58%), Space: 16.8 MB (10.82%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -1,49 +1,32 @@
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-        // int n = s.length();
-        // int maxLength = 0;
-        // unordered_set<char> charSet;
-        // int left = 0;
-        
-        // for (int right = 0; right < n; right++) {
-        //     if (charSet.count(s[right]) == 0) {
-        //         charSet.insert(s[right]);
-        //         maxLength = max(maxLength, right - left + 1);
-        //     } else {
-        //         while (charSet.count(s[right])) {
-        //             charSet.erase(s[left]);
-        //             left++;
-        //         }
-        //         charSet.insert(s[right]);
-        //     }
-        // }
-        
-        // return maxLength;
-
         int n = s.size();
-
-        int maxi = 0;
-        set<char> st;
-        int left = 0;
+        if(n==0) return 0;
+        if(n==1) return 1;
+        int res = 1;
+        int mini = 0;
+        set<int> st;
         for(int i=0;i<n;i++)
         {
             if(st.count(s[i])==0)
             {
                 st.insert(s[i]);
-                maxi = max(maxi,i-left+1);
+                res = max(res,i-mini+1);
             }
             else
             {
                 while(st.count(s[i]))
                 {
-                    st.erase(s[left]);
-                    left = left + 1;
+                    st.erase(s[mini]);
+                    mini = mini + 1;
                 }
                 st.insert(s[i]);
-            }       
-        }
+            }
 
-        return maxi;
+        }
+        return res;
+
+        return res;
     }
 };
