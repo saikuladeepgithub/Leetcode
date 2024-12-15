c43d130cedee6f6c7b7ba29eff210428d8aa9a60
saikuladeepgithub <141644207+saikuladeepgithub@users.noreply.github.com>
2024-12-15 17:53:53 +0530

    Time: 0 ms (100%), Space: 32.6 MB (49.79%) - LeetHub

--- 0966-binary-subarrays-with-sum/0966-binary-subarrays-with-sum.cpp
+++ 0966-binary-subarrays-with-sum/0966-binary-subarrays-with-sum.cpp
@@ -1,25 +1,48 @@
 class Solution {
 public:
+    int func(vector<int> & nums,int goal)
+    {
+        if(goal<0) return 0;
+
+        int l = 0, r = 0;
+        int cnt = 0;
+        int sum = 0;
+        int n = nums.size();
+        while(r<n)
+        {
+            sum += nums[r];
+            while(sum>goal)
+            {
+                sum-=nums[l];
+                l++;
+            }
+            cnt += r-l+1;
+            r++;
+        }
+        return cnt;
+    }
     int numSubarraysWithSum(vector<int>& nums, int goal) {
         
-        int cnt = 0;
-        int prefixsum = 0;
+        // int cnt = 0;
+        // int prefixsum = 0;
 
-        map<int,int> mpp;
+        // map<int,int> mpp;
         
-        mpp[0] = 1;
-        int n = nums.size();
+        // mpp[0] = 1;
+        // int n = nums.size();
 
-        for(int i=0;i<n;i++)
-        {
-            prefixsum += nums[i];
+        // for(int i=0;i<n;i++)
+        // {
+        //     prefixsum += nums[i];
 
-            int remove = prefixsum - goal;
-            cnt += mpp[remove];
+        //     int remove = prefixsum - goal;
+        //     cnt += mpp[remove];
 
-            mpp[prefixsum]++;
-        }
+        //     mpp[prefixsum]++;
+        // }
 
-        return cnt;
+        // return cnt;
+
+        return func(nums,goal) - func(nums,goal-1);
     }
 };
