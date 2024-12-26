c0217f9ac0b1d758768c15c7c60f2f845030b49d
saikuladeepgithub <141644207+saikuladeepgithub@users.noreply.github.com>
2024-12-26 15:19:10 +0530

    Time: 0 ms (100%), Space: 23.1 MB (28.43%) - LeetHub

---
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.cpp
@@ -0,0 +1,29 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int flag = 0;
+
+    int func(TreeNode * root, int l)
+    {
+        if(root==NULL) return l;
+        int lh = func(root->left,l+1);
+        int rh = func(root->right,l+1);
+        if(abs(lh-rh)>1) flag = 1;
+        return max(lh,rh);
+    }
+    bool isBalanced(TreeNode* root) {
+        
+        int temp = func(root,0);
+        return flag==0;
+    }
+};
