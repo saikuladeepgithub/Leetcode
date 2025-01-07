400a1064df533eaa022c6ba2c2c6d6a6391fbc6b
saikuladeepgithub <141644207+saikuladeepgithub@users.noreply.github.com>
2025-01-07 23:26:52 +0530

    Time: 0 ms (100%), Space: 31.4 MB (13.6%) - LeetHub

--- 0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.cpp
+++ 0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.cpp
@@ -15,8 +15,35 @@ public:
     int countNodes(TreeNode* root) {
         if(root == NULL) return 0;
 
-        int l = countNodes(root->left);
-        int r = countNodes(root->right);
-        return 1 + l + r;
+        int l = findheightleft(root);
+        int r = findheightright(root);
+
+        if(l==r) return (1<<l) - 1;
+        return 1 + countNodes(root->left) + countNodes(root->right);
     }
+
+    int findheightleft(TreeNode * root)
+    {
+        int l = 0;
+        while(root)
+        {
+            l++;
+            root = root->left;
+        }
+        return l;
+    }
+
+    int findheightright(TreeNode * root)
+    {
+        int l = 0;
+        while(root)
+        {
+            l++;
+            root = root->right;
+        }
+
+        return l;
+    }
+
+
 };
