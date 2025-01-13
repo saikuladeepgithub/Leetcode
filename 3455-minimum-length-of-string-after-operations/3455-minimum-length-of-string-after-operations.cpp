13db6f7dff316cc058c27347ead4aba9df3beca9
saikuladeepgithub <141644207+saikuladeepgithub@users.noreply.github.com>
2025-01-13 22:38:02 +0530

    Time: 108 ms (27.42%), Space: 32.7 MB (10.89%) - LeetHub

--- 3455-minimum-length-of-string-after-operations/3455-minimum-length-of-string-after-operations.cpp
+++ 3455-minimum-length-of-string-after-operations/3455-minimum-length-of-string-after-operations.cpp
@@ -1,20 +1,67 @@
 class Solution {
 public:
     int minimumLength(string s) {
-        unordered_map <char,int> mpp;
-        int n=s.size();
-        for(int i=0;i<n;i++)
+        
+        // int cnt = s.size();
+        // int n = s.size();
+        // vector<int> vis(n,0);
+
+        // for(int i=0;i<n;i++)
+        // {
+        //     if(vis[i])
+        //         continue;
+        //     int left = -1;
+        //     int right = -1;
+        //     for(int j=0;j<i;j++)
+        //     {
+        //         if(s[j] == s[i])
+        //             left = j;
+        //     }
+        //     if(left==-1)
+        //          continue;
+        //     for(int j = i+1;j<n;j++)
+        //     {
+        //         if(s[j] == s[i])
+        //         {
+        //             right = j;
+        //             break;
+        //         }
+        //     }
+        //     if(right == -1)
+        //         continue;
+        //     vis[left] = vis[right] = 1;
+        //     cnt-=2;
+
+        // }
+
+        // return cnt;
+
+
+
+        int minlength = 0;
+
+        // if the char count is even, then 2 chars will reamain after performing operations
+        // if the char count is odd, then 1 char will remain after performing operations
+
+        map<char,int> mpp;
+        for(int i=0;i<s.size();i++)
         {
             mpp[s[i]]+=1;
+
         }
-        int l=0;
-        for(auto it=mpp.begin();it!=mpp.end();it++)
+
+        for(auto & it : mpp)
         {
-            int count=it->second;
-            if(count>2 && count%2==0) l+=2;
-            else if(count>2 && count%2==1) l+=1;
-            else l+=count;
+            int cnt = it.second;
+            if(cnt>2 && cnt%2)
+                minlength+=1;
+            else if(cnt>2 && !(cnt%2))
+                minlength+=2;
+            else
+                minlength+=cnt;
         }
-        return l;
+
+        return minlength;
+
     }
 };
