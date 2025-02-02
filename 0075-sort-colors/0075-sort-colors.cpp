56638e68d31dbdd61f9325ab486f0de7b6277fd4
saikuladeepgithub <141644207+saikuladeepgithub@users.noreply.github.com>
2025-02-02 23:19:39 +0530

    Time: 0 ms (100%), Space: 11.7 MB (31.55%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -1,68 +1,29 @@
-#include <bits/stdc++.h>
 class Solution {
 public:
     void sortColors(vector<int>& nums) {
-        // for(int i=0;i<nums.size();i++)
-        // {
-        //     for(int j=i;j<nums.size();j++)
-        //     {
-        //         if(nums[i]>nums[j])
-        //         {
-        //             swap(nums[i],nums[j]);
-        //         }
-        //     }
-        // // }
-        // int count0=0;
-        // int count1=0;
-        // int count2=0;
-        // for(int i=0;i<nums.size();i++)
-        // {
-        //     if(nums[i]==0)
-        //     {
-        //         count0=count0+1;
-        //     }
-        //     else if(nums[i]==1)
-        //     {
-        //         count1=count1+1;
-        //     }
-        //     else
-        //     {
-        //         count2=count2+1;
-        //     }
-        // }
-        // for(int i=0;i<count0;i++)
-        // {
-        //     nums[i]=0;
-        // }
-        // for(int i=count0;i<count0+count1;i++)
-        // {
-        //     nums[i]=1;
-        // }
-        // for(int i=count0+count1;i<nums.size();i++)
-        // {
-        //     nums[i]=2;
-        // }
-        // Dutch national flag algorithm
-        int low=0;
-        int mid=0;
-        int high=nums.size()-1;
-        while(mid<=high)
+        
+        int low = 0;
+        int middle = 0;
+        int high = nums.size() - 1;
+        while(middle <= high)
         {
-            if(nums[mid]==0)
+            if(nums[middle] == 0)
             {
-                swap(nums[low],nums[mid]);
-                low=low+1;
-                mid=mid+1;
+                swap(nums[middle],nums[low]);
+                low++;
+                middle++;
             }
-            else if(nums[mid]==1)
+            else if(nums[middle] == 1)
             {
-                mid=mid+1;
+                middle++;
             }
             else
             {
-                swap(nums[mid],nums[high]);
-                high=high-1;
+                swap(nums[high],nums[middle]);
+                high--;
             }
         }
+
+        
     }
 };
